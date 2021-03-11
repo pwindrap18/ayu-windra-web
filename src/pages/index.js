56e59def5015d7cfba6131791e926f014ccc4ec0
pwindrap18 <pwindrap18@gmail.com>
2021-03-12 00:18:08 +0700

    bug

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,7 @@ import React from 'react';
 import Cover from '../components/Cover';
 
 const CommentsPage = ({ setPlaying }) => {
-  return (
-    <div>
-      <Cover setPlaying={setPlaying} />
-    </div>
-  );
+  return <div></div>;
 };
 
 export default CommentsPage;
